42f65f74a84a35b45db84e961c6b16f096b5d6b3
choi-jae-hyuck <cgear@naver.com>
2018-09-17 18:56:47 +0900

    함수 선언
    이동과 애니메이션 함수 생성

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -9,31 +9,36 @@ x=0
 frame=0
 triger=0
 
-def move():
-    pass
+def ani_right(x,y,frame):
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame*100,100,100,100,x,90)
+    update_canvas()
+    frame=(frame+1)%8
+    delay(0.05)
+    get_events()
+def ani_left():
+    clear_canvas(x,y,frame)
+    grass.draw(400,30)
+    character.clip_draw(frame*100,0,100,100,x,90)
+    update_canvas()
+    frame=(frame+1)%8
+    delay(0.05)
+    get_events()
 
-while(True):
-    while(triger==0):
-        clear_canvas()
-        grass.draw(400,30)
-        character.clip_draw(frame*100,100,100,100,x,90)
-        update_canvas()
-        frame=(frame+1)%8
-        x=x+10
-        if(x>800):
-            triger=1
-        delay(0.05)
-        get_events()
-    while(triger==1):
-        clear_canvas()
-        grass.draw(400,30)
-        character.clip_draw(frame*100,0,100,100,x,90)
-        update_canvas()
-        frame=(frame+1)%8
-        x=x-10
-        if(x<0):
-            triger=0
-        delay(0.05)
-        get_events()
+def move(x1,y1,x2,y2):
+    xpos=(x2-x1) // 30
+    ypos=(y2-y1) // 30
+    while(x1!=x2):
+        x1 +=xpos
+        y1 +=ypos
+        if (x1<x2):
+            ani_right(x1,y1,frame)
+        elif (x1>x2):
+            ani_left(x1,y1,frame)
+
+
+        
+        
     
 close_canvas()
