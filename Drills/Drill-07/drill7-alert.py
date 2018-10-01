28a6e5def13aca8716823b20397f2704f84cd1e9
student <heloooooooooooooooooooooooooooooooo>
2018-10-01 17:03:40 +0900

    point set
    set

--- Drills/Drill-07/drill7-alert.py
+++ Drills/Drill-07/drill7-alert.py
@@ -66,43 +66,6 @@ def draw_curve_3_points(p1, p2, p3):
     pass
 
 
-def draw_curve_4_points(p1, p2, p3, p4):
-    draw_big_point(p1)
-    draw_big_point(p2)
-    draw_big_point(p3)
-    draw_big_point(p4)
-
-    # draw p1-p2
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-        draw_point((x, y))
-    draw_point(p2)
-
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        draw_point((x, y))
-    draw_point(p3)
-
-    # draw p3-p4
-    for i in range(50, 100, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
-        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
-        draw_point((x, y))
-    draw_point(p4)
-
-    #draw p4-p1
-    for i in range(50, 100, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p3[0] + (-4 * t ** 2 + 4 * t) * p4[0] + (2 * t ** 2 - t) * p1[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p3[1] + (-4 * t ** 2 + 4 * t) * p4[1] + (2 * t ** 2 - t) * p1[1]
-        draw_point((x, y))
-    draw_point(p1)
 
 def draw_curve_10_points(p1, p2, p3, p4,p5,p6,p7,p8,p9,p10):
     draw_big_point(p1)
@@ -116,100 +79,75 @@ def draw_curve_10_points(p1, p2, p3, p4,p5,p6,p7,p8,p9,p10):
     draw_big_point(p9)
     draw_big_point(p10)
 
-    # draw p1-p2
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
         draw_point((x, y))
     draw_point(p2)
 
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p2[0] + t * p3[0]
+        y = (1 - t) * p2[1] + t * p3[1]
         draw_point((x, y))
     draw_point(p3)
 
-    # draw p3-p4
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p5[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p3[0] + t * p4[0]
+        y = (1 - t) * p3[1] + t * p4[1]
         draw_point((x, y))
     draw_point(p4)
 
-    # draw p4-p5
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p5[0] + (t ** 3 - t ** 2) * p6[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p5[1] + (t ** 3 - t ** 2) * p6[1]) / 2
+    for i in range(0, 20+ 1, 2):
+        t = i / 20
+        x = (1 - t) * p4[0] + t * p5[0]
+        y = (1 - t) * p4[1] + t * p5[1]
         draw_point((x, y))
     draw_point(p5)
 
-    # draw p5-p6
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p6[0] + (t ** 3 - t ** 2) * p7[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p6[1] + (t ** 3 - t ** 2) * p7[1]) / 2
+    for i in range(0, 20+ 1, 2):
+        t = i / 20
+        x = (1 - t) * p5[0] + t * p6[0]
+        y = (1 - t) * p5[1] + t * p6[1]
         draw_point((x, y))
     draw_point(p6)
 
-    # draw p6-p7
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p5[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p7[0] + (t ** 3 - t ** 2) * p8[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p5[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p7[1] + (t ** 3 - t ** 2) * p8[1]) / 2
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p6[0] + t * p7[0]
+        y = (1 - t) * p6[1] + t * p7[1]
         draw_point((x, y))
     draw_point(p7)
 
-    # draw p7-p8
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p6[0] + (-4 * t ** 2 + 4 * t) * p7[0] + (2 * t ** 2 - t) * p8[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p6[1] + (-4 * t ** 2 + 4 * t) * p7[1] + (2 * t ** 2 - t) * p8[1]
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p7[0] + t * p8[0]
+        y = (1 - t) * p7[1] + t * p8[1]
         draw_point((x, y))
     draw_point(p8)
 
-    # draw p8-p9
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p7[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p9[0] + (t ** 3 - t ** 2) * p10[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p7[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p9[1] + (t ** 3 - t ** 2) * p10[1]) / 2
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p8[0] + t * p9[0]
+        y = (1 - t) * p8[1] + t * p9[1]
         draw_point((x, y))
     draw_point(p9)
 
-    # draw p9-p10
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p8[0] + (-4 * t ** 2 + 4 * t) * p9[0] + (2 * t ** 2 - t) * p10[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p8[1] + (-4 * t ** 2 + 4 * t) * p9[1] + (2 * t ** 2 - t) * p10[1]
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p9[0] + t * p10[0]
+        y = (1 - t) * p9[1] + t * p10[1]
         draw_point((x, y))
     draw_point(p10)
 
-    # draw p10-p1
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p9[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p10[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p9[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p10[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+    for i in range(0, 20 + 1, 2):
+        t = i / 20
+        x = (1 - t) * p10[0] + t * p1[0]
+        y = (1 - t) * p10[1] + t * p1[1]
         draw_point((x, y))
     draw_point(p1)
-
 
 
 prepare_turtle_canvas()
